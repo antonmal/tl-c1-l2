b8eca4ec7c77dfe4911427b6d65dc6c7a8d73902
Anton Malkov <anton.malkov@gmail.com>
2016-02-16 17:20:57 +0100

    Extract move methods from Player to TTT class

--- ttt_oop.rb
+++ ttt_oop.rb
@@ -4,108 +4,12 @@ require 'colorize'
 # Tic-Tac-Toe OOP game for the Tealeaf Course C1-L2
 # (Anton Malkov)
 
-# Creates a player attached to a particular board and using a specified marker
+# Creates a player and specifies his marker
 class Player
-  attr_accessor :marker, :board
+  attr_accessor :marker
 
-  def initialize(marker, board)
+  def initialize(marker)
     @marker = marker
-    @board = board
-  end
-end
-
-# Creates a human player and let's him choose moves via command line
-class Human < Player
-  def move
-    prompt_for_move
-    loop do
-      the_move = gets.chomp.downcase
-      if board.empty_squares.include? the_move
-        board[the_move] = marker
-        break
-      end
-      ask_to_choose_an_empty_square
-    end
-  end
-
-  private
-
-  def prompt_for_move
-    puts "\n=> Where do you want to move?"
-    puts "   (type row letter followed by the column number (like 'B2')"
-  end
-
-  def ask_to_choose_an_empty_square
-    puts "\n=> Please, choose one of the following options:"
-    puts board.empty_squares.join(', ')
-  end
-end
-
-# Creates a computer player and let's him choose a random move
-#   or smart move using the minimax algorythm
-class Computer < Player
-  DUMB_MOVE_PROBABILITY = 10
-
-  def move
-    if rand(100) <= DUMB_MOVE_PROBABILITY
-      board[random_move] = marker
-    else # otherwise use minimax algorythm
-      board[smart_move] = marker
-    end
-  end
-
-  private
-
-  def random_move
-    board.empty_squares.sample
-  end
-
-  def smart_move
-    weights = all_move_weights(board, marker)
-    best_weight = weights.values.max
-    weights.select { |_, v| v == best_weight }.keys.sample
-  end
-
-  def all_move_weights(board_state, next_marker)
-    weights = {}
-    board_state.empty_squares.each do |move|
-      new_board_state = Board.new board_state.squares_with(move, next_marker)
-      weights.merge! move => minimax(new_board_state, next_marker)
-    end
-    weights
-  end
-
-  def move_weight(board_state)
-    if board_state.winning_marker == marker
-      return 1
-    elsif board_state.someone_won? # not nil and not equal to computer marker
-      return -1
-    else
-      return 0
-    end
-  end
-
-  def minimax(board_state, current_marker)
-    # If game is over because someone won or because the board is full
-    # then stop weighing subsecuent moves and return the weight of the last move
-    if board_state.full? || board_state.someone_won?
-      return move_weight(board_state)
-    end
-
-    # Cycle markers for each subsequent move
-    next_move_marker = the_other_marker(current_marker)
-
-    # Weight all possible subsecuent moves
-    move_weights = all_move_weights(board_state, next_move_marker)
-    if next_move_marker == marker # It's computer's move. Choose the best one.
-      return move_weights.values.max
-    else # It's human's move. Choose the one that is worst for the computer.
-      return move_weights.values.min
-    end
-  end
-
-  def the_other_marker(current_marker)
-    (TTT.markers - [current_marker]).first
   end
 end
 
@@ -190,16 +94,13 @@ class TTT
   HUMAN_MARKER = 'X'.green
   COMPUTER_MARKER = 'O'.red
   FIRST_TO_MOVE = HUMAN_MARKER
+  COMPUTER_DUMB_MOVE_PROBABILITY = 10
 
   def initialize
     @board = Board.new
-    @player = Human.new(HUMAN_MARKER, board)
-    @computer = Computer.new(COMPUTER_MARKER, board)
+    @player = Player.new(HUMAN_MARKER)
+    @computer = Player.new(COMPUTER_MARKER)
     @current_marker = FIRST_TO_MOVE
-  end
-
-  def self.markers
-    [HUMAN_MARKER, COMPUTER_MARKER]
   end
 
   def play
@@ -213,6 +114,10 @@ class TTT
   end
 
   private
+
+  def markers
+    [HUMAN_MARKER, COMPUTER_MARKER]
+  end
 
   def clear
     system('clear') || system('cls')
@@ -252,14 +157,96 @@ class TTT
   def current_player_moves
     case current_marker
     when HUMAN_MARKER
-      player.move
+      human_move
       @current_marker = COMPUTER_MARKER
     when COMPUTER_MARKER
-      computer.move
+      computer_move
       @current_marker = HUMAN_MARKER
     end
     clear
     puts board
+  end
+
+  def human_move
+    prompt_for_move
+    loop do
+      the_move = gets.chomp.downcase
+      if board.empty_squares.include? the_move
+        board[the_move] = player.marker
+        break
+      end
+      ask_to_choose_an_empty_square
+    end
+  end
+
+  def prompt_for_move
+    puts "\n=> Where do you want to move?"
+    puts "   (type row letter followed by the column number (like 'B2')"
+  end
+
+  def ask_to_choose_an_empty_square
+    puts "\n=> Please, choose one of the following options:"
+    puts board.empty_squares.join(', ')
+  end
+
+  def computer_move
+    if rand(100) <= COMPUTER_DUMB_MOVE_PROBABILITY
+      board[random_move] = computer.marker
+    else
+      board[smart_move] = computer.marker
+    end
+  end
+
+  def random_move
+    board.empty_squares.sample
+  end
+
+  def smart_move
+    weights = all_move_weights(board, computer.marker)
+    best_weight = weights.values.max
+    weights.select { |_, v| v == best_weight }.keys.sample
+  end
+
+  def all_move_weights(board_state, next_marker)
+    weights = {}
+    board_state.empty_squares.each do |move|
+      new_board_state = Board.new board_state.squares_with(move, next_marker)
+      weights.merge! move => minimax(new_board_state, next_marker)
+    end
+    weights
+  end
+
+  def move_weight(board_state)
+    if board_state.winning_marker == computer.marker
+      return 1
+    elsif board_state.winning_marker == player.marker
+      return -1
+    else
+      return 0
+    end
+  end
+
+  def minimax(board_state, current_marker)
+    # If game is over because someone won or because the board is full
+    # then stop weighing subsecuent moves and return the weight of the last move
+    if board_state.full? || board_state.someone_won?
+      return move_weight(board_state)
+    end
+
+    # Cycle markers for each subsequent move
+    next_move_marker = the_other_marker(current_marker)
+
+    # Weight all possible subsecuent moves
+    move_weights = all_move_weights(board_state, next_move_marker)
+    if next_move_marker == computer.marker
+      return move_weights.values.max # Choose the best move for computer.
+    else # It's human's move.
+      return move_weights.values.min # Choose the best move for human.
+    end
+  end
+
+  def the_other_marker(current_marker)
+    (markers - [current_marker]).first
   end
 
   # rubocop:disable Metrics/AbcSize
